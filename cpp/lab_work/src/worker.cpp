02d0be27721b5283f77cc8ae40acf6e1b259e339
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-06-13 10:08:08 +0300

    [feature] IM: добавил название должности и метод, её возвращающий в класс

--- cpp/lab_work/src/worker.cpp
+++ cpp/lab_work/src/worker.cpp
@@ -4,6 +4,10 @@ std::string Worker::get_name() {
     return name_;
 }
 
+std::string Worker::get_position() {
+    return position_;
+}
+
 uint16_t Worker::get_employment_year() {
     return employment_year_;
 }
