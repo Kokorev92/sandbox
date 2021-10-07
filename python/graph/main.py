f2460cb0d702fc07292069d39032a815ca98ea0c
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-10-07 23:13:21 +0300

    [refactor]: new data for visualization

--- python/graph/main.py
+++ python/graph/main.py
@@ -7,15 +7,20 @@ FILE = "log.csv"
 time = []
 humidity = []
 temperature = []
+light = []
 
 with open(FILE, "r") as file:
     data = csv.reader(file)
     for row in data:
         time.append(row[0])
         humidity.append(row[2])
-        temperature.append(row[1]) 
+        temperature.append(row[1])
+        light.append(row[3])
 
 time = [dt.datetime.strptime(i, "%H:%M:%S") for i in time]
 
-plt.plot(time, humidity, time,  temperature)
+fig, ax = plt.subplots()
+ax.plot(time, humidity)
+ax.plot(time, temperature)
+ax.plot(time, light)
 plt.show()
