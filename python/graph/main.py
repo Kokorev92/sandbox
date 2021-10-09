0ef7db4569f997a0c7e1df51e8a85fa5e5852367
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-10-09 09:03:22 +0300

    [feature]: преобразовал значения АЦП освещения в бинарное состояние - вкл/выкл

--- python/graph/main.py
+++ python/graph/main.py
@@ -1,7 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import matplotlib.ticker as ticker
 import datetime as dt
 
 FILE = "log.csv"
@@ -10,6 +9,15 @@ time = []
 humidity = []
 temperature = []
 light = []
+
+
+# Функция для преобразования значения освещения
+def light_status(current_light):
+    if current_light > 200:
+        return 1
+    else:
+        return 0
+
 
 # Считываем все значения в соответствующие списки
 # Списки будут иметь тип string, после мы преобразуем их в int
@@ -26,6 +34,9 @@ humidity = list(map(int, humidity))
 temperature = list(map(int, temperature))
 light = list(map(int, light))
 
+# Преобразуем АЦП значения освещения к двум состояниям: 1 - вкл. 0 - выкл.
+light = list(map(light_status, light))
+
 # Преобразуем время к типу datetime
 time = [dt.datetime.strptime(i, "%H:%M:%S") for i in time]
 
@@ -33,9 +44,9 @@ fig, ax = plt.subplots()
 ax.set_title('Bathroom telemetry')  # Заголовок графика
 ax.set_xlabel('Time(H:M:S)')  # Подпись оси X
 ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))  # Формат подписи меток времени по оси X
-ax.legend()  # Отображение легенды
 
-ax.plot(time, light, label='Light')
+ax.plot(time, light, label='Light (on/off)', color='red')
 ax.plot(time, temperature, label='Temperature (C)')
 ax.plot(time, humidity, label='Humidity (%)')
+ax.legend()  # Отображение легенды
 plt.show()
