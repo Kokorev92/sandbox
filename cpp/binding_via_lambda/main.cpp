3ccd6b9f2e784d1e386532a53daa252f4aa9c941
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-11-23 18:08:45 +0300

    [feature]: пример связывания классов через лямбду

---
+++ cpp/binding_via_lambda/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+class A {
+public:
+    A(){}
+
+    float get_temp() {
+        return temp_;
+    }
+private:
+    float temp_ = 0.10;
+};
+
+class B {
+public:
+
+    template <typename T>
+    B(const T& func) : func_(func){}
+
+    void print(){
+        cout << func_();
+    }
+
+private:
+    std::function<float()> func_;
+};
+
+int main() {
+
+    A a;
+
+    B b([&a] () -> float {return a.get_temp();});
+
+    b.print();
+
+    return 0;
+}
