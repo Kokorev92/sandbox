8b4a5ad6299c0639a4ab079e3f5d54a35077b1f8
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-12-11 11:29:07 +0300

    [feature]: вывод таблицы в консоль в стиле даташитов. Lib PrettyTable

--- python/bits_get.py
+++ python/bits_get.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 
 import sys
+from prettytable import PrettyTable
 
 if len(sys.argv) > 1:
-    bits = sys.argv[1]
+    try:
+        num = int(sys.argv[1])
+    except:
+        print('Please enter an integer')
+        sys.exit(-1)
 else:
     sys.exit(-1)
 
-try:
-    num = int(bits)
-except:
-    print('Please enter an integer')
-    sys.exit(-1)
+output_table = PrettyTable()
+output_table.add_column("Bit", ["State"])
 
 bits_count = len(bin(num)) - 2
 
-i = 0
-while i < bits_count:
-    print("Bit " + str(i) + ": " + str(bool(num & (1 << i))))
-    i += 1
+for i in reversed(range(bits_count)):
+    output_table.add_column(str(i), [str(1 if bool(num & (1 << i)) else 0)])
+
+print(output_table)
