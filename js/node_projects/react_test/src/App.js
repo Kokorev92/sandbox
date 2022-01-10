04722209406e3e53d37bb0ba1a01dcc6ae8f27bb
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-01-10 14:46:24 +0300

    [wip]: добавил костыли

--- js/node_projects/react_test/src/App.js
+++ js/node_projects/react_test/src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
+import animate from './animate';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        < div className="App" >
+            <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/"><img src="logo.png" style={{ width: 180, height: 70 }} /></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключатель навигации">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="navbar navbar-nav navbar-right" id="navbarNav">
+                        <form class="d-flex">
+                            <canvas id="canvas"></canvas>
+                            <input class="form-control me-2" type="search" placeholder="Поиск" aria-label="Поиск" />
+                            <button class="btn btn-outline-success" type="submit">Поиск</button>
+                        </form>
+                    </div>
+                </div>
+            </nav >
+
+            <main class="content-wrapper">"
+                <div class="container h-100 d-flex flex-wrap justify-content-center" style={{ "padding-top": "90px", "padding-bottom": "90px" }}>
+                    <div class="row row-cols-4 mb-2 mt-2 gx-5">
+                        <div class="col">
+                            <div class="card " style={{ width: "18rem" }}>
+                                <img src="1.png" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Товар 1</h5>
+                                    <p class="card-text">Описание товара</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card " style={{ width: "18rem" }}>
+                                <img src="2.png" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Товар 2</h5>
+                                    <p class="card-text">Описание товара</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card " style={{ width: "18rem" }}>
+                                <img src="3.png" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Товар 3</h5>
+                                    <p class="card-text">Описание товара</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card " style={{ width: "18rem" }}>
+                                <img src="4.png" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Товар 4</h5>
+                                    <p class="card-text">Описание товара</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row-cols-4 mb-2 g-5">
+                        <div class="col">
+                            <div class="card " style={{ width: "18rem" }}>
+                                <img src="5.png" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Товар 5</h5>
+                                    <p class="card-text">Описание товара</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card " style={{ width: "18rem" }}>
+                                <img src="6.png" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Товар 6</h5>
+                                    <p class="card-text">Описание товара</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card " style={{ width: "18rem" }}>
+                                <img src="7.png" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Товар 7</h5>
+                                    <p class="card-text">Описание товара</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card " style={{ width: "18rem" }}>
+                                <img src="8.png" class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Товар 8</h5>
+                                    <p class="card-text">Описание товара</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </main >
+            <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50 fixed-bottom">
+                <div class="container text-center">
+                    <small>Copyright &copy; Кокорев Я.С. 1045М</small>
+                </div>
+            </footer>
+        </div >
+    );
 }
 
 export default App;
---
+++ js/node_projects/react_test/src/animate.js
@@ -0,0 +1,76 @@
+class Sprite {
+    constructor(options) {
+        this.ctx = options.ctx;
+
+        this.image = options.image;
+
+        this.frameIndex = 0;
+        this.tickCount = 0;
+        this.ticksPerFrame = options.ticksPerFrame || 0;
+        this.numberOfFrames = options.numberOfFrames || 1;
+
+        this.width = options.width;
+        this.height = options.height;
+
+        this.start();
+    }
+
+    update() {
+        this.tickCount++;
+
+        if (this.tickCount > this.ticksPerFrame) {
+            this.tickCount = 0;
+            if (this.frameIndex < this.numberOfFrames - 1) {
+                this.frameIndex++;
+            } else {
+                this.frameIndex = 0;
+            }
+        }
+    }
+
+    render() {
+        this.ctx.clearRect(0, 0, this.width / this.numberOfFrames, this.height);
+        this.ctx.drawImage(
+            this.image,
+            this.frameIndex * this.width / this.numberOfFrames,
+            0,
+            this.width / this.numberOfFrames,
+            this.height,
+            0,
+            0,
+            this.width / this.numberOfFrames,
+            this.height
+        )
+    }
+
+    start() {
+        let loop = () => {
+            this.update();
+            this.render();
+
+            window.requestAnimationFrame(loop);
+        }
+
+        window.requestAnimationFrame(loop);
+    }
+}
+
+function animate() {
+    let canvas = document.getElementById('canvas');
+    canvas.width = 64;
+    canvas.height = 64;
+
+    let coinImage = new Image();
+    coinImage.src = 'coin-sprite-animation.png';
+
+    let sprite = new Sprite({
+        ctx: canvas.getContext('2d'),
+        image: coinImage,
+        width: 508,
+        height: 64,
+        numberOfFrames: 8,
+        ticksPerFrame: 4,
+    })
+}
+
+export default animate;
