85807238a58569967d68b4f30928074138035af2
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-03-03 15:10:51 +0300

    [wip]: заготовка функции конкантенации строк, изменил немного функцию strlen

--- cpp/str_functions/main.cpp
+++ cpp/str_functions/main.cpp
@@ -2,17 +2,21 @@
 
 using namespace std;
 
+namespace my {
 unsigned strlen(const char* str) {
     unsigned len = 0;
-    while (str[len] != '\0') {
-        len++;
+    while (str[++len] != '\0') {
     }
     return len;
 }
 
+void strcat(char* to, const char* from) {}
+}  // namespace my
+
 char str[] = "fuck off!";
 
 int main() {
-    cout << strlen(str);
+    cout << my::strlen(str) << endl;
+    my::strcat(str, str);
     return 0;
 }
