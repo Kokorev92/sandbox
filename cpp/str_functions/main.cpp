c5ecedfc8171ba4ae35f3e0f9ebe8403ecee4c25
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-03-03 14:51:55 +0300

    [wip]: реализовал функцию strlen

--- cpp/str_functions/main.cpp
+++ cpp/str_functions/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+unsigned strlen(const char* str) {
+    unsigned len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+char str[] = "fuck off!";
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << strlen(str);
     return 0;
 }
