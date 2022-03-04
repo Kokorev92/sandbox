c453be648bc38d071d3cacf65a938d2b192c09f2
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-03-04 08:54:19 +0300

    [wip]: реализовал функцию strcat, пофиксил strlen

--- cpp/str_functions/main.cpp
+++ cpp/str_functions/main.cpp
@@ -5,18 +5,30 @@ using namespace std;
 namespace my {
 unsigned strlen(const char* str) {
     unsigned len = 0;
-    while (str[++len] != '\0') {
+    while (str[len] != '\0') {
+        len++;
     }
     return len;
 }
 
-void strcat(char* to, const char* from) {}
+void strcat(char* to, const char* from) {
+    unsigned len = strlen(to);
+    for (unsigned i = 0; i < strlen(from); ++i) {
+        to[len + i] = from[i];
+    }
+    to[len + strlen(from)] = '\0';
+}
 }  // namespace my
 
-char str[] = "fuck off!";
+char str[15] = "fuck off!";
+char str2[]  = " bitch";
 
 int main() {
     cout << my::strlen(str) << endl;
-    my::strcat(str, str);
+    my::strcat(str, str2);
+
+    for (int i = 0; i < my::strlen(str); ++i) {
+        cout << str[i];
+    }
     return 0;
 }
